5c87ab83d9b6e90675c53517564d5e341bd655f1
comus <comus.leong@gmail.com>
2022-10-14 17:42:21 +0800

    update

--- src/client/app.js
+++ src/client/app.js
@@ -109,7 +109,7 @@ async function renderPrediction() {
 
   if (canRender) {
     // canvas render
-    // camera.canvas.requestRenderAll();
+    camera.canvas.requestRenderAll();
   }
 
   requestAnimationFrame(renderPrediction);
